f9137bba5966e015f8341b59790237e565d4506a
DESKTOP-AFVU23C\CSIE_AIOT_04 <hanson90136@gmail.com>
2021-09-06 16:04:54 +0800

    init

---
+++ DockerRegistryAPI/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DockerRegistryAPI.Models
+{
+    public class TestModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
